2f3994a9e12243d169628e4b0cea0eb2c66fb17b
Ynda Jas <yndajas@gmail.com>
2020-07-05 15:33:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  converted_string = ""
+  if array.length == 1
+    converted_string = array.join
+  elsif array.length == 2
+    converted_string = "#{array[0]} and #{array[1]}"
+  else
+    items_left = array.length
+    until items_left == 1 do
+      converted_string << "#{array[array.length - items_left]}, "
+      items_left -= 1
+    end
+    converted_string << "and #{array.last}"
+  end
+  converted_string
 end
+
